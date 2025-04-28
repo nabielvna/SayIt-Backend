312e6c2567c5f3e5ca8744bd075e15f5ef4bad8e
nabielvna <nabielvna@gmail.com>
2025-04-28 14:20:35 +0700

    deploy test commit

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,9 @@
 import app from "@/app";
-import env from "@/env";
+// import env from "@/env";
 
-const port = env.PORT;
-console.log(`Server is running on http://localhost:${port}`);
+// const port = env.PORT;
 
 export default {
-  port,
+  // port,
   fetch: app.fetch,
 };
